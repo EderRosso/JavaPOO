57db202a01b4b6a5308f7afc03e2dbf841ff50de
EderRosso <ederderosso@rede.ulbra.br>
2022-05-05 17:06:19 -0300

    aula06

---
+++ aula06-Encapsulamento/src/aula06/encapsulamento/Aula06Encapsulamento.java
@@ -0,0 +1,19 @@
+package aula06.encapsulamento;
+
+
+public class Aula06Encapsulamento {
+
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        
+
+
+        c.abrirMenu();
+        
+        c.desligar();
+
+    }
+    
+}
